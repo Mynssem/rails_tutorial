c470b6e81fa6204b8610f3bdc777e57edb30864d
Mynssem <rafaelmynssem@id.uff.br>
2018-02-11 13:29:58 -0200

    exercicio do rails tutorial sobre deploy - 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Alo, mundo!"
+    render html: "Alo, Mundo!"
   end
 
   #função "goodbye" com exclamação invertida (exclamation mark)
